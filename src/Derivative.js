eae5e0a57168179b46364d7e80201a98d0ceda80
Tyler Cako <tylercako@Tylers-MacBook-Air.local>
2022-12-08 18:23:11 -0700

    Initialized some stuff and got React working

---
+++ src/Derivative.js
@@ -0,0 +1,13 @@
+import "./Derivative.css";
+
+export default function Derivative(){
+    return(
+        <div id="Derivative">
+            This is derivative object
+            <div className="Input-field">
+                <label for="number">Enter Derivative Here:</label>
+                <input name="number" type=""></input>
+            </div>
+        </div>
+    )
+}
